6cddc029807280920f832a8e7c7c251483cc8f63
arkadii <88792842+Arkadii-2021@users.noreply.github.com>
2022-08-20 16:16:57 +0700

    add oop -hw

--- src/js/app.js
+++ src/js/app.js
@@ -24,53 +24,53 @@ export class Character {
       throw new Error();
     }
   }
-};
+}
 
 export class Bowerman extends Character {
-  constructor(name, health, level) {
+  constructor(name) {
     super(name);
-	this.type = "Bowerman";
+    this.type = 'Bowerman';
     this.attack = 25;
     this.defence = 25;
   }
-};
+}
 export class Swordsman extends Character {
-  constructor(name, health, level) {
+  constructor(name) {
     super(name);
-	this.type = "Swordsman";
+    this.type = 'Swordsman';
     this.attack = 40;
     this.defence = 10;
   }
-};
+}
 export class Magician extends Character {
-  constructor(name, health, level) {
+  constructor(name) {
     super(name);
-	this.type = "Magician";
+    this.type = 'Magician';
     this.attack = 10;
     this.defence = 40;
   }
-};
+}
 export class Daemon extends Character {
-  constructor(name, health, level) {
+  constructor(name) {
     super(name);
-	this.type = "Daemon";
+    this.type = 'Daemon';
     this.attack = 10;
     this.defence = 40;
   }
-};
+}
 export class Undead extends Character {
-  constructor(name, health, level) {
+  constructor(name) {
     super(name);
-	this.type = "Undead";
+    this.type = 'Undead';
     this.attack = 25;
     this.defence = 25;
   }
-};
+}
 export class Zombie extends Character {
-  constructor(name, health, level) {
+  constructor(name) {
     super(name);
-	this.type = "Zombie";
+    this.type = 'Zombie';
     this.attack = 40;
     this.defence = 10;
   }
-};
+}
